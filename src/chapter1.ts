81a47cf5cd1578ffd128e4e1d09230c3d296acc8
yzooop <qkrduswn0404@gmail.com>
2025-08-05 23:34:15 +0900

    section2 원시타입과 리터럴타입

---
+++ src/chapter1.ts
@@ -0,0 +1,25 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let string1: string = "Hello, World!";
+let string2: string = 'Hello, World!';
+let string3: string = `Hello, World!`;
+let string4: string = `Hello, ${string3}`;
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// 리터럴 타입
+let numA: 10 = 10;
+let strA: "Hello" = "Hello";
+let boolA: true = true;
