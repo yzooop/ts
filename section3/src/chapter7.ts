c482c59028cc7464cb02bb934c6b34829145f62e
yzooop <qkrduswn0404@gmail.com>
2025-08-27 20:09:05 +0900

    section3 타입 좁히기

---
+++ section3/src/chapter7.ts
@@ -0,0 +1,24 @@
+// 타입 좁히기
+// 조건문 등을 이용해 넓은 타입에서 좁은 타입으로
+// 타입을 상황에 따라 좁히는 방법을 이야기
+
+type Person = {
+  name: string;
+  age: number;
+}
+
+// value => number : toFixed
+// value => string : toUpperCase
+// value => Date : getTime
+// value => Person : name은 age살 입니다.
+function func(value: number | string | Date | Person) {
+  if (typeof value === "number") { // type guard 라고 부른다.
+    console.log(value.toFixed())
+  } else if (typeof value === "string") {
+    console.log(value.toUpperCase())
+  } else if (value instanceof Date) {
+    console.log(value.getTime())
+  } else if (value && 'age' in value) {
+    console.log(`${value.name}은  ${value.age}살 입니다.`)
+  }
+}
